3b79717702b15b27ecaa280fb78b4b88f61ccd1b
Aldy Rahman <aldy.rahman02@gmail.com>
2025-08-29 11:09:12 +0800

    Feat: Frontend Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-2xl text-blue-600'>Lorem ipsum</h1>
+    </div>
+  )
+}
+
+export default App
